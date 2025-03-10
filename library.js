001ae13279b010ff9dbac201baa680eb59c66950
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-03-09 19:45:51 -0700

    basic table initialization with existing array contents

--- library.js
+++ library.js
@@ -8,6 +8,26 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.getId = function () {
+    return this.id;
+}
+
+Book.prototype.getTitle = function () {
+    return this.title;
+}
+
+Book.prototype.getAuthor = function () {
+    return this.author;
+}
+
+Book.prototype.getPages = function () {
+    return this.pages;
+}
+
+Book.prototype.getRead = function () {
+    return this.read;
+}
+
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
@@ -16,3 +36,43 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary("Myths from Mesopotamia", "Stephanie Dalley", 339, false);
 addBookToLibrary("example 1", "fake author", 230, true);
 addBookToLibrary("example 2", "other author", 12, false);
+
+const table = document.querySelector("#books");
+
+function initialize() {
+    for (const book of myLibrary) {
+        let title = book.getTitle();
+        const titleCell = document.createElement("td");
+        titleCell.classList.add("titleCell");
+        titleCell.textContent = title;
+
+        let author = book.getAuthor();
+        const authorCell = document.createElement("td");
+        authorCell.classList.add("authorCell");
+        authorCell.textContent = author;
+
+        let pages = book.getPages().toString();
+        const pagesCell = document.createElement("td");
+        pagesCell.classList.add("pagesCell");
+        pagesCell.textContent = pages;
+
+        let read = book.getRead() ? "Yes" : "No";
+        const readCell = document.createElement("td");
+        readCell.classList.add("readCell");
+        readCell.textContent = read;
+
+        let id = book.getId();
+        const newRow = document.createElement("tr");
+        newRow.classList.add("book");
+        newRow.id = id;
+
+        newRow.appendChild(titleCell);
+        newRow.appendChild(authorCell);
+        newRow.appendChild(pagesCell);
+        newRow.appendChild(readCell);
+
+        table.appendChild(newRow);
+    }
+}
+
+initialize();

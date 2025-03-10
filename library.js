dafbd3055d63beffa4f766de839f59d037ddad03
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-03-10 10:54:27 -0700

    now adding as cards

--- library.js
+++ library.js
@@ -38,6 +38,7 @@ addBookToLibrary("example 1", "fake author", 230, true);
 addBookToLibrary("example 2", "other author", 12, false);
 
 const table = document.querySelector("#books");
+const grid = document.querySelector("main");
 
 function initializeTable() {
     for (const book of myLibrary) {
@@ -75,4 +76,51 @@ function initializeTable() {
     }
 }
 
+function initializeCards() {
+    for (const book of myLibrary) {
+        const xIcon = document.createElement("img");
+        xIcon.src = "./assets/icons/alpha-x-circle-outline.svg";
+        xIcon.alt = "X Icon";
+        xIcon.classList.add("xCircle");
+        const xButton = document.createElement("button");
+        xButton.type = "button";
+        xButton.classList.add("removeButton");
+        xButton.appendChild(xIcon);
+
+        let title = book.getTitle();
+        const titleParagraph = document.createElement("p");
+        titleParagraph.classList.add("title");
+        titleParagraph.textContent = title;
+
+        let author = book.getAuthor();
+        const authorParagraph = document.createElement("p");
+        authorParagraph.classList.add("author");
+        authorParagraph.textContent = author;
+
+        let pages = book.getPages().toString();
+        const pagesParagraph = document.createElement("p");
+        pagesParagraph.classList.add("pages");
+        pagesParagraph.textContent = pages;
+
+        let read = book.getRead() ? "Read" : "Unread";
+        const readParagraph = document.createElement("p");
+        readParagraph.classList.add("read");
+        readParagraph.textContent = read;
+
+        let id = book.getId();
+        const newCard = document.createElement("div");
+        newCard.classList.add("card");
+        newCard.id = id;
+
+        newCard.appendChild(xButton);
+        newCard.appendChild(titleParagraph);
+        newCard.appendChild(authorParagraph);
+        newCard.appendChild(pagesParagraph);
+        newCard.appendChild(readParagraph);
+
+        grid.appendChild(newCard);
+    }
+}
+
 //initializeTable();
+initializeCards();
